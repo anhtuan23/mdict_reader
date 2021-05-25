c709bd4ae3b6c5cb2663b9e31020912c9ca6abf5
Do Tuan Anh <dotuananh.dta@gmail.com>
2021-05-25 22:25:24 +0700

    misc

--- bin/example.dart
+++ bin/example.dart
@@ -2,10 +2,10 @@ import 'package:mdict_reader/src/mdict_manager.dart';
 
 void main() async {
   final mdxPaths = [
-    // 'dict/CC-CEDICT.mdx',
-    // 'dict/JaViDic_Ja-Vi.mdx',
-    // 'dict/OALD_8th.mdx',
-    'dict/jmdict.mdx',
+    'dict/CC-CEDICT.mdx',
+    'dict/JaViDic_Ja-Vi.mdx',
+    'dict/OALD_8th.mdx',
+    // 'dict/jmdict.mdx',
   ];
   final word = '勉強';
   // final word = '哽咽';
